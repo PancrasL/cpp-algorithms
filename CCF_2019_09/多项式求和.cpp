19ade04976bc610e45a10c80a638ebba03971cb2
lvpengze <103349015@qq.com>
2019-08-22 16:13:31 +0800

    增加了注释

--- CCF_2019_09/多项式求和.cpp
+++ CCF_2019_09/多项式求和.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-double ans[1000];
-void getAns()
-{
-    ans[1] = 1;
-    for (int i = 2; i < 1000; i++) {
-        if (i & 1) {
-            ans[i] = ans[i - 1] + 1.0 / i;
-        } else {
-            ans[i] = ans[i - 1] - 1.0 / i;
-        }
-    }
-}
-int main(void)
-{
-    getAns();
-    int m, n;
-    cin >> m;
-    while (m--) {
-        cin >> n;
-        printf("%.2f\n", ans[n]);
-    }
-    return 0;
-}
+// #include <iostream>
+// #include <stdio.h>
+// using namespace std;
+// double ans[1000];
+// void getAns()
+// {
+//     ans[1] = 1;
+//     for (int i = 2; i < 1000; i++) {
+//         if (i & 1) {
+//             ans[i] = ans[i - 1] + 1.0 / i;
+//         } else {
+//             ans[i] = ans[i - 1] - 1.0 / i;
+//         }
+//     }
+// }
+// int main(void)
+// {
+//     getAns();
+//     int m, n;
+//     cin >> m;
+//     while (m--) {
+//         cin >> n;
+//         printf("%.2f\n", ans[n]);
+//     }
+//     return 0;
+// }
