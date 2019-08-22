6909688ea189b42c13af590702bb1dad3f561732
lvpengze <103349015@qq.com>
2019-08-22 15:35:40 +0800

    简单求积，可使用&运算符判断奇偶

---
+++ CCF_2019_09/求奇数的乘积.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+// int main(void)
+// {
+//     int n;
+//     while (cin >> n) {
+//         long long ans = 1;
+//         for (int i = 0; i < n; i++) {
+//             int temp;
+//             cin >> temp;
+//             if (temp & 1)
+//                 ans *= temp;
+//         }
+//         cout << ans << endl;
+//     }
+//     return 0;
+// }
