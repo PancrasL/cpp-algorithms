a25d90e1d698d9294c85348a90d7a57fea9178b7
lvpengze <103349015@qq.com>
2019-08-23 09:50:41 +0800

    使用set和istringstream

--- CCF_2019_09/ACM题型/Step1.入门/基础训练/单词数.cpp
+++ CCF_2019_09/ACM题型/Step1.入门/基础训练/单词数.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <set>
+#include <sstream>
 using namespace std;
 int main(void)
 {
-    
+    string line;
+    while (getline(cin, line)) {
+        if (line == "#")
+            break;
+        istringstream inStr(line);
+        set<string> S;
+        string word;
+        while (inStr >> word) {
+            S.insert(word);
+        }
+        cout << S.size() << endl;
+    }
+    return 0;
 }
